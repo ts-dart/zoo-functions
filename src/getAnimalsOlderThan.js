6ae4071338976c039e3ec2bb49ec5ab409363378
Thiago <thiagoedusan5.11@outlook.com>
2022-01-26 00:33:58 -0300

    Requisito2 ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const obj = data.species.filter((currObj) => currObj.name === animal);
+  const list = obj[0].residents.filter((currAge) => currAge.age < age);
+
+  if (list.length === 0) return true;
+  return false;
 }
 
 module.exports = getAnimalsOlderThan;
