60fd76281f3106d169b1ed0b424dbba77a5d6760
Thiago <thiagoedusan5.11@outlook.com>
2022-01-26 15:17:47 -0300

    Requisito 3 ok

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) return {};
+
+  const obj = data.employees.find((curr) => curr.firstName === employeeName
+  || curr.lastName === employeeName);
+
+  return obj;
 }
 
 module.exports = getEmployeeByName;
