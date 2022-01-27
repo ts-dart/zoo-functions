af78edbfc36fbed09cf71c4cdce0d9cdfad94e30
Thiago <thiagoedusan5.11@outlook.com>
2022-01-27 15:49:09 -0300

    Requisito 5 ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,23 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const obj = {};
+    const animals = data.species.map((curr) => {
+      obj[curr.name] = curr.residents.length;
+      return obj;
+    });
+    return animals[0];
+  }
+
+  const popularity = data.species.find((curr) => curr.name === animal.specie);
+
+  if (animal.sex) {
+    const find = popularity.residents.filter((curr) => curr.sex === animal.sex);
+    return find.length;
+  }
+
+  return popularity.residents.length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,17 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const managers = data.employees.map((curr) => curr.managers);
+  // const compare = managers.find((curr) => {
+  //   console.log(curr === id);
+  //   console.log(curr, id);
+  // });
+  console.log(id);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
 
+console.log(isManager('stephanieId'));
 module.exports = { isManager, getRelatedEmployees };
