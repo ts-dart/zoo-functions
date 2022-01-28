a3b3f266092233cc5df4cd534d0b5e7e693720e0
Thiago <thiagoedusan5.11@outlook.com>
2022-01-27 23:05:32 -0300

    Requisito 6 ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,26 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const childrens = entrants.filter((curr) => curr.age < 18);
+  const adults = entrants.filter((curr) => curr.age >= 18 && curr.age < 50);
+  const seniors = entrants.filter((curr) => curr.age >= 50);
+
+  return {
+    child: childrens.length,
+    adult: adults.length,
+    senior: seniors.length,
+  };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+
+  const visitors = countEntrants(entrants);
+  const count = [visitors.child * 20.99, visitors.adult * 49.99, visitors.senior * 24.99];
+  const sum = count.reduce((acc, curr) => acc + curr);
+
+  return sum;
 }
 
 module.exports = { calculateEntry, countEntrants };
