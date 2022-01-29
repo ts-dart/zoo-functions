891ed73a909faa10052e97a95bbd4a3a6b5f744e
Thiago <thiagoedusan5.11@outlook.com>
2022-01-29 13:55:00 -0300

    Requisito 4 ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,17 +2,26 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  const managers = data.employees.map((curr) => curr.managers);
-  // const compare = managers.find((curr) => {
-  //   console.log(curr === id);
-  //   console.log(curr, id);
-  // });
-  console.log(id);
+  const manager = data.employees.map((obj) => obj.managers.filter((mId) => mId === id));
+  const m = manager.some((curr) => curr[0] === id);
+  return m;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const array = data.employees.map((obj) => {
+    const bol = obj.managers.some((curr) => curr === managerId);
+    let lintB;
+    if (bol) lintB = obj.firstName;
+    return `${lintB} ${obj.lastName}`;
+  });
+  const arrNomes = array.map((curr) => curr.split(' '));
+  const noUndefined = arrNomes.filter((curr) => curr[0] !== 'undefined');
+  const result = noUndefined.map((curr) => curr.join(' '));
+  return result;
 }
 
-console.log(isManager('stephanieId'));
 module.exports = { isManager, getRelatedEmployees };
