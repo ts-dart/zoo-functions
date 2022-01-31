5b0c71e14717eef40759ee87dc38c241f8436a66
Thiago <thiagoedusan5.11@outlook.com>
2022-01-31 11:00:34 -0300

    Requisito 9 ok

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,19 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = data.employees.find((obj) => obj.id === id);
+  const firstSpecieId = employee.responsibleFor[0];
+  const specie = data.species.find((obj) => obj.id === firstSpecieId);
+
+  let result;
+  let maior = 0;
+  for (let i = 0; i < specie.residents.length; i += 1) {
+    if (specie.residents[i].age > maior) {
+      maior = specie.residents[i].age;
+      result = [specie.residents[i].name, specie.residents[i].sex, specie.residents[i].age];
+    }
+  }
+  return result;
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (scheduleTarget === undefined) {
+    const days = Object.keys(data.hours).map((curr) => {
+      return { [curr]: {
+        officeHour: `Open from ${data.hours[curr].open} until ${data.hours[curr].close}`,
+        exhibition: 0,
+      } };
+    });
+    console.log(days);
+  }
 }
 
 module.exports = getSchedule;
