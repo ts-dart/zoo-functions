be960c43aa1cefebdab085a5fe32b371d93580fe
Thiago <thiagoedusan5.11@outlook.com>
2022-02-01 13:23:42 -0300

    Resolvendo rquisito 8

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,6 +1,6 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+function getEmployeesCoverage(param) {
   // seu código aqui
 }
 
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,42 @@
 const data = require('../data/zoo_data');
 
+function allDays() {
+  const objeto = {};
+  const days = Object.keys(data.hours).map((curr) => {
+    const avail = data.species.map((obj) => {
+      let nam;
+      if (obj.availability.some((day) => day === curr)) nam = obj.name;
+      return nam;
+    });
+    objeto[curr] = {
+      officeHour: `Open from ${data.hours[curr].open}am until ${data.hours[curr].close}pm`,
+      exhibition: avail.filter((names) => names !== undefined),
+    };
+    objeto.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    return objeto;
+  });
+  return days[0];
+}
+
+function invalidParam(param) {
+  let invalid = true;
+
+  Object.keys(data.hours).forEach((day) => {
+    if (day === param) invalid = false;
+  });
+  data.species.forEach((obj) => {
+    if (obj.name === param) invalid = false;
+  });
+
+  return invalid;
+}
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
-  if (scheduleTarget === undefined) {
-    const days = Object.keys(data.hours).map((curr) => {
-      return { [curr]: {
-        officeHour: `Open from ${data.hours[curr].open} until ${data.hours[curr].close}`,
-        exhibition: 0,
-      } };
-    });
-    console.log(days);
+  if (scheduleTarget === undefined || invalidParam(scheduleTarget)) return allDays();
+
+  if (scheduleTarget === 'Monday') {
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
   }
 }
 
